1ce1c04b51841d74cfaa7ec04a519e5c2a4f18c2
Shiv Chauhan <shivchauhan795@gmail.com>
2025-03-28 23:56:14 +0530

    added backend apis

---
+++ backend/CONTRIBUTION.md
@@ -0,0 +1,5 @@
+## Prisma Commands for Contribution
+
+To generate prisma migration: `npx prisma generate`
+
+To migrate prisma migration: `npm prisma migrate dev`
---
+++ backend/api/validate/validator.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const validateUserSignup = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(20),
+    name: z.string()
+})
+
+
+export const validateUserLogin = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(20),
+})
