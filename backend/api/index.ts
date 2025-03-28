29195837f751f694b7c4a77fcb7477aa659e0e5c
Shiv Chauhan <shivchauhan795@gmail.com>
2025-03-28 11:45:54 +0530

    init

---
+++ backend/api/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center">
+      hi
+    </div>
+  )
+}
+
+export default App
