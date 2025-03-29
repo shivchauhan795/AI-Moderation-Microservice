929b0bd9094921e7cd94a91517246ecec9e682e2
Shiv Chauhan <shivchauhan795@gmail.com>
2025-03-29 15:52:12 +0530

    docker files added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
-})
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
